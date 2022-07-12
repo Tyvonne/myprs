76945902a3ab349d3d61fe20b02831ad4b3c2cb8
TAVERNIER Thibaut <thibaut.tavernier@3ds.com>
2022-07-12 09:57:25 +0200

    Started app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import { Chart } from "react-google-charts";
 
+export const data = [
+  ["Date", "Hang power clean [kg]"],
+  ["29/04/2021", 63],
+  ["24/06/2022", 65],
+  ["11/07/2022", 75],
+];
+
+export const options = {
+  isStacked: true,
+  height: 300,
+  legend: { position: "top", maxLines: 3 },
+  vAxis: { minValue: 0 },
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
+        <header className="App-header">
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 2 }}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  My PR's
+                </Typography>
+                <Button color="inherit">Login</Button>
+              </Toolbar>
+            </AppBar>
+          </Box>
+        </header>
+      </div>
+      <div>
         <Chart
-          chartType="ScatterChart"
-          data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+          chartType="AreaChart"
           width="100%"
           height="400px"
-          legendToggle
+          data={data}
+          options={options}
         />
-      </header>
+      </div>
     </div>
   );
 }
